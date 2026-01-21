794bedf1e7c2160ea20d5fa0f6502287c4b0e862
quarkusio <no-reply@quarkus.io>
2026-01-21 17:22:34 +0000

    Initial commit

---
+++ src/test/java/cv/sousa/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package cv.sousa;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
